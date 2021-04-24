38cd7d3c6620c4ff4f273cb180c094e64d4efa04
Facundo <facundo.figliuolo@method.com.ar>
2021-04-24 15:42:40 -0300

    first commit

---
+++ app/src/main/java/com/ppc/ejercicios/EJ/FragmentEjercicio1.java
@@ -0,0 +1,86 @@
+package com.ppc.ejercicios.EJ;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.jakewharton.rxbinding4.view.RxView;
+import com.ppc.ejercicios.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FragmentEjercicio1 extends Fragment {
+
+    @BindView(R.id.button_cambiar_texto)
+    Button cambiarTexto;
+    @BindView(R.id.button_aceptar)
+    Button aceptar;
+    @BindView(R.id.button_cancelar)
+    Button cancelar;
+
+    @BindView(R.id.vistaTexto)
+    TextView texto;
+    @BindView(R.id.editarTexto)
+    EditText editarTexto;
+
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_ejercicio_1, container, false);
+        ButterKnife.bind(this, view);
+
+        aceptar.setVisibility(View.INVISIBLE);
+        cancelar.setVisibility(View.INVISIBLE);
+        editarTexto.setVisibility(View.INVISIBLE);
+
+        RxView.clicks(cambiarTexto)
+                .subscribe(aVoid -> {
+                    aceptar.setVisibility(View.VISIBLE);
+                    cancelar.setVisibility(View.VISIBLE);
+                    editarTexto.setVisibility(View.VISIBLE);
+
+                    cambiarTexto.setVisibility(View.INVISIBLE);
+                    texto.setVisibility(View.INVISIBLE);
+                });
+
+        RxView.clicks(aceptar)
+                .subscribe(aVoid -> {
+                    aceptar.setVisibility(View.INVISIBLE);
+                    cancelar.setVisibility(View.INVISIBLE);
+                    editarTexto.setVisibility(View.INVISIBLE);
+                    texto.setText(editarTexto.getText());
+
+                    cambiarTexto.setVisibility(View.VISIBLE);
+                    texto.setVisibility(View.VISIBLE);
+                });
+
+        RxView.clicks(cancelar)
+                .subscribe(aVoid -> {
+                    aceptar.setVisibility(View.INVISIBLE);
+                    cancelar.setVisibility(View.INVISIBLE);
+                    editarTexto.setVisibility(View.INVISIBLE);
+
+                    cambiarTexto.setVisibility(View.VISIBLE);
+                    texto.setVisibility(View.VISIBLE);
+                });
+
+        return view;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/ppc/ejercicios/EJ/FragmentPrincipalEjercicios.java
@@ -0,0 +1,53 @@
+package com.ppc.ejercicios.EJ;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.jakewharton.rxbinding4.view.RxView;
+import com.ppc.ejercicios.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FragmentPrincipalEjercicios extends Fragment {
+
+    @BindView(R.id.button_ejercicio_1)
+    Button ejercicio1;
+    @BindView(R.id.button_ejercicio_2)
+    Button ejercicio2;
+    @BindView(R.id.button_ejercicio_3)
+    Button ejercicio3;
+
+
+    NavController navController;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_ejercicios, container, false);
+        ButterKnife.bind(this, view);
+
+        NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.fragment);
+        navController = navHostFragment.getNavController();
+
+        RxView.clicks(ejercicio1)
+                .subscribe(aVoid -> {
+                    navController.navigate(R.id.fragment_ejercicio_1);
+                });
+
+        return view;
+}
+
+}
